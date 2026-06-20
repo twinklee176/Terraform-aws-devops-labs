15ccd00230f7269cf1d70099a3b25a19c6a2f902
Twinkle Mahato <twinkle8340@gmail.com>
2026-06-20 20:35:10 +0530

    Create output.tf

---
+++ Task-4-Terraform-Variables/output.tf
@@ -0,0 +1,27 @@
+output "region" {
+  value = var.aws_region
+}
+
+output "instance_name" {
+  value = var.instance_name
+}
+
+output "instance_type" {
+  value = var.instance_type
+}
+
+output "ami_id" {
+  value = var.ami_id
+}
+
+output "users" {
+  value = var.users
+}
+
+output "map_example" {
+  value = var.instance_types
+}
+
+output "object_example" {
+  value = var.server_config
+}
